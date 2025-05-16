479d6abb353a6a6961e67984c435ef879cfc684c
divyanshubse <bansaldivyanshu721@hmail.com>
2025-05-16 18:54:44 +0530

    confilct scenario

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,10 +8,10 @@ public class App {
         System.out.println("Hello World!");
         String name="Ashok";
         int i=10;
-<<<<<<< HEAD
+
         int j=20;
-=======
+
         String name1="Divyanshu";
->>>>>>> e6a30e6dbbbf0bbbe6e71378542c552100148039
+        boolean found=false;
     }
 }
