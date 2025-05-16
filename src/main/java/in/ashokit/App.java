5ec571fe00f784c5f6126187aed661e32e40e43f
divyanshubse <bansaldivyanshu721@hmail.com>
2025-05-16 19:10:16 +0530

    removing unused variables

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -6,15 +6,6 @@ package in.ashokit;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String name="Ashok";
-        int i=10;
-      int j=20;
-
-        String name1="Divyanshu";
-        boolean found=false;
-        int j1=20;
-
-        String name2="Divyanshu";
-        char x='A';
+       
     }
 }
