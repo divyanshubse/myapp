7b710cec02c60cd8f4d1f40f1d05c38733036bf0
divyanshubse <bansaldivyanshu721@hmail.com>
2025-06-21 21:38:17 +0530

    101 is completed

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,8 +8,15 @@ public class App {
         System.out.println("Hello World!");
        int i=10;
     }
+<<<<<<< Updated upstream
 //java 102
 public void m2(){
 System.out.println("story 102");
+=======
+
+public void m1(){
+ int i=10;
+ int j=20;
+>>>>>>> Stashed changes
 }
 }
